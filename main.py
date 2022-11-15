ebed1744db0a639d22ea9b3dc4e7840039357c49
Krupa-v <117572761+Krupa-v@users.noreply.github.com>
2022-11-15 21:32:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-print("calculator")
-print("1.add")
-print("2.substract")
-print("3.multiply")
-print("4.divide")
-ch=int(input("enter choic(1-4):"))
-if ch==1:
-    a=int(input("enter A:"))
-    b=int(input("enter B:"))
-    c=a+b
-    print("sum = ",c)
-elif ch==2:
-    a=int(input("enter A:"))
-    b=int(input("enter B:"))
-    c=a-b
-    print("difference = ",c)
-elif ch==3:
-    a=int(input("enter A:"))
-    b=int(input("enter B:"))
-    c=a*b
-    print("product = ",c)
-elif ch==4:
-    a=int(input("enter A:"))
-    b=int(input("enter B:"))
-    c=a/b
-    print("quotient = ",c)
-else:
-    print("invalid choice")
+n=int(input("enter number of rows needed:"))
+for i in range(n) :
+    for j in range(i+1) :
+     print(j+1,end=" ")
+     print()
+
